2513746751baec83cddcf4af6b0a83c102f66313
tmytng <tmytng.2020@gmail.com>
2020-12-03 00:18:03 +0900

    create destory-action

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,4 +1,6 @@
 class ToDosController < ApplicationController
+  before_action :set_to_do, only: [:edit, :update, :destroy]
+
   def index
     @to_dos = ToDo.all
   end
@@ -14,27 +16,32 @@ class ToDosController < ApplicationController
     else
       render :new
     end
+  end
 
   def edit
-    @to_do = ToDo.find_by(id: params[:id])
   end
 
   def update
-    @to_do = ToDo.find_by(id: params[:id])
     if @to_do.update(to_do_params)
       redirect_to to_dos_url
     else
       render :edit
     end
+  end
 
- end
-
-end
+  def destroy
+    @to_do.destroy
+    redirect_to to_dos_url
+  end
 
 private
 
   def to_do_params
     params.require(:to_do).permit(:title)
+  end
+
+  def set_to_do
+    @to_do = ToDo.find_by(id: params[:id])
   end
 
 end
